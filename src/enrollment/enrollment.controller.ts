7bba8fa221cbe149a54e15980263324465803bfc
Kayjer-Islam <kaizerislam577@gmail.com>
2026-04-05 19:53:04 +0600

    Lab Task 03 Dependency Injection

---
+++ src/enrollment/enrollment.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { EnrollmentService } from './enrollment.service';
+
+@Controller('enrollment')
+export class EnrollmentController {
+
+  constructor(private enrollmentService: EnrollmentService) {}
+
+  @Get()
+  getAll() {
+    return this.enrollmentService.getEnrollments();
+  }
+
+  @Post()
+  enroll(@Body() body: any) {
+    const { studentName, courseId } = body;
+    return this.enrollmentService.enrollStudent(studentName, courseId);
+  }
+}
